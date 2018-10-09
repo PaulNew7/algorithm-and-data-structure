36d8fb9e24e3e3629fdaff0b0cdb12528fdb351f
paul_computer <lovegod2011@163.com>
2018-10-09 21:49:38 +0800

    BT 构建，镜像，遍历

---
+++ b_tree.py
@@ -0,0 +1,89 @@
+#encoding=utf8
+'''
+二叉树相关
+'''
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+def bulid(pre, mid):
+    '''
+    pre: 前序遍历
+    mid: 中序遍历
+    '''
+    if not pre:
+        return None
+    r=pre[0]
+    new=TreeNode(r)
+    index=mid.index(r)
+    new.left=bulid(pre[1:index+1],mid[:index])
+    new.right=bulid(pre[index+1:],mid[index+1:])
+    return new
+
+def pre_run(r):
+    '''
+    前序遍历
+    '''
+    if not r:
+        return ''
+    return str(r.val)+pre_run(r.left)+pre_run(r.right)
+
+
+def mid_run(r): # todo error!
+    '''
+    中序遍历
+    '''
+    if not r:
+        return ''
+    return mid_run(r.left)+str(r.val)+mid_run(r.right)
+
+
+
+class Solution:
+    # 返回镜像树的根节点
+    def Mirror(self, root):
+        # write code here
+        def gen(r):
+            if not r:
+                return None
+            new=TreeNode(r.val)
+            if r.left:
+                new.right=gen(r.left)
+            if r.right:
+                new.left=gen(r.right)
+            return new
+        return gen(root)
+
+
+    def Mirror2(self, root):
+        if root != None:
+            root.left,root.right = root.right,root.left
+            self.Mirror(root.left)
+            self.Mirror(root.right)
+        return root
+
+if __name__ == '__main__':
+    print '------------------start'
+    def gen(ttype='single'):
+        pre=['a','b','e','f','c','d','g']
+        mid=['e','b','f','a','d','g','c']
+        t1=bulid(pre,mid)
+        try:
+            assert pre_run(t1)==''.join(pre) and mid_run(t1)==''.join(mid), 'check tree error'
+        except:
+            print pre_run(t1), mid_run(t1)
+
+        if ttype=='single':
+            return t1
+        else:
+            raise Exception('no support')
+
+    t=gen()
+    print '------------------',t
+
+    mirror=Solution().Mirror(t)
+    mirror2=Solution().Mirror2(t)
+    print '--------',pre_run(mirror), mid_run(mirror)
+    print '--------',pre_run(mirror2), mid_run(mirror2)
